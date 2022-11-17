4ff8ea30b385d97baaaff5ccab1e97e5ab707da5
jbijlsma <jeroen_work@yahoo.com>
2022-11-17 08:09:44 +0800

    Initial commit

---
+++ Dnw.WebShop.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dnw.WebShop.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
