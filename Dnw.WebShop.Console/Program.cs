c1b822067130445a58bc2b722a730f4ae794c5d6
jbijlsma <jeroen_work@yahoo.com>
2022-11-17 13:48:22 +0800

    Add console app that logs top selling products to the console

---
+++ Dnw.WebShop.Console/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Based on: https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-usage
+
+using System.Reflection;
+using Dnw.WebShop.Core.Services;
+using Dnw.WebShop.Infrastructure.ExternalApi.ChannelEngine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        var configBuilder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json", false, true)
+            .AddUserSecrets(Assembly.GetExecutingAssembly());
+
+        services.AddScoped<IProductService, ProductService>();
+        services.AddChannelEngineService(configBuilder.Build());
+    }).Build();
+
+await ExemplifyScoping(host.Services);
+
+await host.RunAsync();
+
+static async Task ExemplifyScoping(IServiceProvider services)
+{
+    using var serviceScope = services.CreateScope();
+    var provider = serviceScope.ServiceProvider;
+
+    var productService = provider.GetRequiredService<IProductService>();
+    var topSellingProducts = (await productService.GetTopSellingProducts(5)).ToList(); 
+    Console.WriteLine();
+    Console.WriteLine($"Found {topSellingProducts.Count} top selling products:");
+    Console.WriteLine();
+    
+    var productNames = string.Join(Environment.NewLine, topSellingProducts.Select((p, index) => $"{index+1} => {p.ProductName} ({p.TotalQuantity})"));
+    Console.WriteLine(productNames);
+    Console.WriteLine();
+    
+    var randomIndex = Random.Shared.Next(0, topSellingProducts.Count);
+    var randomProduct = topSellingProducts[randomIndex];
+    Console.WriteLine($"Selected random product: {randomProduct.ProductName}");
+    Console.WriteLine();
+
+    const int newStock = 24;
+    Console.WriteLine($"Updating stock to : {newStock}");
+
+    Console.WriteLine($"Stock updated to : {newStock}");
+
+    Console.WriteLine();
+    Console.WriteLine("Press any key to exit ..");
+    Console.ReadLine();
+}
